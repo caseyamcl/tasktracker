07971e10463d11febdeb831703dd5d15946fcd36
Casey McLaughlin <caseyamcl@gmail.com>
2015-03-18 15:05:04 -0400

    Added getMemUsage and getMemPeakUsage to Report Interface

--- src/Report.php
+++ src/Report.php
@@ -1,11 +1,28 @@
 <?php
 
+/**
+ * Tack Tracker - A library for tracking long-running task progress
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/tasktracker
+ * @version 2.0
+ * @package caseyamcl/tasktracker
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * ------------------------------------------------------------------
+ */
+
 namespace TaskTracker;
 
 use TaskTracker\Helper\MagicPropsTrait;
 
 /**
- * Report Value Object
+ * Task Tracker Report
+ *
+ * Represents a snapshot of task state from a specific time*
  *
  * @author Casey McLaughlin <caseyamcl@gmail.com>
  */
